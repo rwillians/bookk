b8a10d5a33a534093d6de15e927069a629913115
Rafael Willians <me@rwillians.com>
2023-10-24 13:26:03 -0300

    Exporting custom formatter rules for :book

--- .formatter.exs
+++ .formatter.exs
@@ -1,9 +1,14 @@
+locals_without_parens = [
+  credit: 2,
+  debit: 2,
+  journalize!: 2
+]
+
 [
   inputs: ["{config,lib,test}/**/*.{ex,exs}"],
   line_length: 100,
-  locals_without_parens: [
-    credit: 2,
-    debit: 2,
-    journalize!: 2
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
   ]
 ]
