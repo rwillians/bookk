1abd82f9379959bf4799c2aa008b97624ca6008d
Rafael Willians <me@rwillians.com>
2025-08-12 15:07:59 -0300

    Added callback class/1 to chart of accounts

--- test/support/test_chart_of_accounts.ex
+++ test/support/test_chart_of_accounts.ex
@@ -1,10 +1,8 @@
 defmodule TestChartOfAccounts do
-  @moduledoc false
-
-  @behaviour Bookk.ChartOfAccounts
+  use Bookk.ChartOfAccounts
 
   alias Bookk.AccountClass, as: C
-  alias Bookk.AccountHead, as: Head
+  alias Bookk.AccountHead, as: H
 
   @classes %{
     "CA" => %C{id: "CA", parent_id: "A", natural_balance: :debit},
@@ -13,13 +11,14 @@ defmodule TestChartOfAccounts do
   }
 
   @impl Bookk.ChartOfAccounts
+  def class(<<id::binary>>), do: Map.get(@classes, id)
+
+  @impl Bookk.ChartOfAccounts
   def ledger(:acme), do: "acme"
   def ledger({:user, user_id}), do: "user(#{user_id})"
 
   @impl Bookk.ChartOfAccounts
-  def account(:cash), do: %Head{name: "cash/CA", class: @classes["CA"]}
-  def account(:deposits), do: %Head{name: "deposits/OE", class: @classes["OE"]}
-
-  def account({:unspent_cash, {:user, user_id}}),
-    do: %Head{name: "unspent-cash:user(#{user_id})/L", class: @classes["L"]}
+  def account(:cash), do: %H{name: "cash/CA", class: class("CA")}
+  def account(:deposits), do: %H{name: "deposits/OE", class: class("OE")}
+  def account({:unspent_cash, {:user, user_id}}), do: %H{name: "unspent-cash:user(#{user_id})/L", class: class("L")}
 end
